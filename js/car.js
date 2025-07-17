d5616bab5368195ecaab45f1a905a6915fd163f8
Muzahidul Islam Saki <122040712+muzahidulsaki@users.noreply.github.com>
2025-07-18 00:00:18 +0600

    Update car.js

--- js/car.js
+++ js/car.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded",  () {
+document.addEventListener("DOMContentLoaded",  function() {
   const preview = document.getElementById("carPreview");
   const previewImage = document.getElementById("previewImage");
   const previewName = document.getElementById("previewName");
